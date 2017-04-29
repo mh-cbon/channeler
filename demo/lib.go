136a6fe43c42cd6031d4fb00111291e94ab4484f
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-29 17:44:41 +0200

    update demo

--- demo/lib.go
+++ demo/lib.go
@@ -2,10 +2,12 @@ package demo
 
 import "fmt"
 
-//go:generate channeler tomate_gen.go Tomate:MyTomate
+//go:generate channeler tomate_gen.go Tomate:MyTomate *Tomate:MyTomatePointer
 
 // Tomate is a vegetable.
-type Tomate struct{}
+type Tomate struct {
+	name string
+}
 
 // Hello world!
 func (t *Tomate) Hello() { fmt.Println(" world!") }
@@ -15,3 +17,10 @@ func (t Tomate) Good() { fmt.Println(" bye!") }
 
 // Name it!
 func (t Tomate) Name(it string) string { return fmt.Sprintf("Name:%v\n", it) }
+
+// NewTomate isa contrstuctor
+func NewTomate(n string) *Tomate {
+	return &Tomate{
+		name: n,
+	}
+}
